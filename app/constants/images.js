9a23939d5d81264b5de1d4ff2c51c1990f301548
lowkeyboard <caqlarybak@gmail.com>
2022-05-14 13:19:30 +0300

    manually added fonts are removed [CP]

--- app/constants/images.js
+++ app/constants/images.js
@@ -1,7 +1,9 @@
 export const applogo = require('../../assets/applogo.png');
 export const googlelogo = require('../../assets/googlelogo.png');
+export const headerlogo = require('../../assets/headerlogo.png');
 
 export default {
   applogo,
   googlelogo,
+  headerlogo,
 };
--- app/navigation/TabNav.js
+++ app/navigation/TabNav.js
@@ -1,16 +1,36 @@
 import React, {useEffect, useRef} from 'react';
 
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import {Icon} from '@rneui/themed';
 
 import Profile from '../pages/tabpages/Profile';
 import Home from '../pages/tabpages/Home';
+import {images, SIZES, COLORS} from '../../constants';
 
 const Drawer = createDrawerNavigator();
 
 export default function TabNav() {
   return (
     <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Tune24',
+          drawerIcon: ({focused, size}) => (
+            <Icon name="g-translate" color="#00aced" />
+          ),
+          headerStyle: {
+            backgroundColor: '#FFFFFF',
+          },
+          headerLeftLabelVisible: false,
+          headerTintColor: '#000000',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleContainerStyle: {},
+        }}
+      />
       <Drawer.Screen name="Profile" component={Profile} />
     </Drawer.Navigator>
   );
