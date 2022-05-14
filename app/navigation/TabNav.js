82755f2df4e1a7118997c892d360a7f55c801cea
lowkeyboard <caqlarybak@gmail.com>
2022-05-14 15:44:16 +0300

    appbar changed

--- app/navigation/TabNav.js
+++ app/navigation/TabNav.js
@@ -2,10 +2,10 @@ import React, {useEffect, useRef} from 'react';
 
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {Icon} from '@rneui/themed';
-
+import {Text, View, StyleSheet, Image} from 'react-native';
 import Profile from '../pages/tabpages/Profile';
 import Home from '../pages/tabpages/Home';
-import {images, SIZES, COLORS} from '../../constants';
+import {images, SIZES, COLORS} from '../constants';
 
 const Drawer = createDrawerNavigator();
 
@@ -16,22 +16,51 @@ export default function TabNav() {
         name="Home"
         component={Home}
         options={{
-          title: 'Tune24',
-          drawerIcon: ({focused, size}) => (
-            <Icon name="g-translate" color="#00aced" />
-          ),
+          // title: 'Home',
+          drawerIcon: ({focused, size}) => <Icon name="home" color="#00aced" />,
           headerStyle: {
             backgroundColor: '#FFFFFF',
           },
           headerLeftLabelVisible: false,
           headerTintColor: '#000000',
-          headerTitleStyle: {
-            fontWeight: 'bold',
+          headerTitle: () => (
+            <View style={styles.headerView}>
+              <Image
+                source={images.headerlogo}
+                style={{
+                  padding: 10,
+                  margin: 5,
+                  height: 30,
+                  width: 40,
+                  resizeMode: 'contain',
+                }}
+              />
+
+              <Text style={styles.appTextBar}>Tune24</Text>
+            </View>
+          ),
+
+          headerTitleContainerStyle: {
+            elevation: 2,
           },
-          headerTitleContainerStyle: {},
         }}
       />
       <Drawer.Screen name="Profile" component={Profile} />
     </Drawer.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  appTextBar: {
+    color: COLORS.blue,
+    fontSize: 17,
+    fontFamily: 'Helvetica',
+    fontWeight: '700',
+  },
+  headerView: {
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
